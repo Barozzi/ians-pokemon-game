0ca0dfe8fc259c379a1bad91abc07a9016e67b8e
Gregory Barozzi <gregbarozzi@gmail.com>
2020-02-06 07:39:29 -0800

    Configure and deploy to firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase/app";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var firebaseConfig = {
+  apiKey: "AIzaSyABtx_WhXZn5sPvz3sojsUTSubfPLZ7Mv4",
+  authDomain: "ians-pokemon-game.firebaseapp.com",
+  databaseURL: "https://ians-pokemon-game.firebaseio.com",
+  projectId: "ians-pokemon-game",
+  storageBucket: "ians-pokemon-game.appspot.com",
+  messagingSenderId: "636055702141",
+  appId: "1:636055702141:web:d22a10732233566ac13f14",
+  measurementId: "G-KV3GHRQRL6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
